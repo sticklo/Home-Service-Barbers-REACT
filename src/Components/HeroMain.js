5cf19a1d00b78ef9a5f0f5e4fe19aeed00d9161b
sticklo <mafimisebiadebayo@gmail.com>
2021-08-11 18:49:16 +0100

    initial comit

---
+++ src/Components/HeroMain.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import "../App.css";
+
+function Navbar (){
+    return (
+        <section className="HeroMain">
+            <div className="Header">
+                <div className="Navbar">
+                    <div className="Logo">
+                        <a>H <span>S</span> B</a>
+                    </div>
+                     <div className="NavLinks">
+                         <button id="HamburgerBtn">&#9776;</button>
+                     </div>
+                 </div>
+            </div>
+            <div className="HeroSection">
+                <div className="HeroHeader">
+                    <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h1>
+                </div>
+                <div className="HeroParagraph">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Proin libero nibh, lacinia consequat viverra vel, dapibus id lectus.</p>
+                </div>
+                <div className="HeroLinks">
+                    <button className="FirstBtn">Book A Haircut</button>
+                    <button className="SecondBtn">See Barbers List</button>
+              </div>
+            </div>
+        </section>
+        
+    )
+}
+export default Navbar;
